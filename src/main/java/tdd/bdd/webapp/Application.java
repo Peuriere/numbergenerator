f8b3567882cc36fc8644a2a4c2bb7fb3a5351ee5
freekvandooren <f_van_dooren@hotmail.com>
2017-01-30 21:28:13 +0100

    Some overlooked configuration

--- src/main/java/tdd/bdd/webapp/Application.java
+++ src/main/java/tdd/bdd/webapp/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("samster.sam")
+@ComponentScan("tdd.bdd")
 public class Application {
 
     public static void main(String[] args) {
